91b0ea3a814e9eca50a0eaf2326ae63e3b9b7aa7
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-02-07 15:28:53 +0700

    create admin controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function(){
+
+    Route::group(['prefix' => 'article'], function(){
+        route::get('/', 'ArticleController@index')->name='articles.index';
+    });
+    
+});
