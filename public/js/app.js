7310d69d01c4c866eae3116aa059a664ec529503
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-02-19 12:38:11 +0700

    tinymce assets

--- public/js/app.js
+++ public/js/app.js
@@ -49685,8 +49685,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/Data/Project/PHP/my_personal_blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Volumes/Data/Project/PHP/my_personal_blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Volumes/Data/Project/Laravel/my_personal_blog/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Volumes/Data/Project/Laravel/my_personal_blog/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <script src="{{ asset('node_modules/tinymce/tinymce.js') }}"></script>
     <script>
         tinymce.init({
             selector:'textarea.content',
