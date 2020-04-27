4c25bef0bde6e4817e1c450427daea69c1120e2f
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-04-28 02:22:18 +0700

    jquery modal

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -60,6 +60,27 @@
                     </div>
 
                     <div class="form-group row">
+                        <label for="excerpt" class="col-md-2 col-form-label text-md-right">{{ __('Image') }}</label>
+                        <div class="col-md-9">
+                            <div class="custom-file">
+                                <button type="button" class="" id="myBtn" name="image" return false;>Image</button>
+                                {{-- <label class="custom-file-label" for="image">Choose file</label> --}}
+
+                                <!-- The Modal -->
+                                <div id="myModal" class="modalku">
+
+                                    <!-- Modal content -->
+                                    <div class="modal-contentku">
+                                    <span class="close">&times;</span>
+                                    <p>Some text in the Modal..</p>
+                                    </div>
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
                         <label for="excerpt" class="col-md-2 col-form-label text-md-right">{{ __('Excerpt') }}</label>
 
                         <div class="col-md-9">
@@ -174,4 +195,33 @@
         </div> --}}
     </div>
 </div>
+
+<script type="text/javascript">
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+</script>
+
 @endsection
