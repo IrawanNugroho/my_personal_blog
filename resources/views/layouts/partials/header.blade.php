ef2799ee112cb96dbab1e6a0172d3e5d6e8e9ad8
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2021-02-26 03:03:23 +0700

    update header

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,15 @@
+<!-- start section header -->
+<div id="header" class="home">
+    <div class="container">
+        <div class="header-content">
+            <h1>I'm <span class="typed"></span></h1>
+            <p>PHP, Laravel, Lumen, WordPress; Python, Django, Flask</p>
+
+            <ul class="list-unstyled list-social">
+            <li><a href="https://github.com/IrawanNugroho"><i class="ion-social-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/irawan-setyo-adhi-nugroho-4a5018b6/"><i class="ion-social-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End section header -->
